0ae7ff53dcfc67b494832c10a91140e0f33bd5db
Dams4K <dams4k@users.noreply.github.com>
2026-05-15 21:09:43 +0200

    maybe

---
+++ custom_build.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+basedir=$(cd $(dirname "$0"); pwd)
+
+source $basedir/setup.sh
+
+if [ -z "$1" -o -z "$2" ]; then
+  echo "Usage: $0 <godot branch> <base distro>"
+  echo
+  echo "Example: $0 4.x f39"
+  echo
+  echo "godot branch:"
+  echo "        Informational, tracks the Godot branch these containers are intended for."
+  echo
+  echo "base distro:"
+  echo "        Informational, tracks the base Linux distro these containers are based on."
+  echo
+  echo "The resulting image version will be <godot branch>-<base distro>."
+  exit 1
+fi
+
+godot_branch=$1
+base_distro=$2
+img_version=$godot_branch-$base_distro
+files_root="$basedir/files"
+
+if [ ! -z "$PS1" ]; then
+  # Confirm settings
+  echo "Docker image tag: ${img_version}"
+  echo
+  while true; do
+    read -p "Is this correct? [y/n] " yn
+    case $yn in
+      [Yy]* ) break;;
+      [Nn]* ) exit 1;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+fi
+
+mkdir -p logs
+
+"$podman" build -t godot-fedora:${img_version} -f Dockerfile.base . 2>&1 | tee logs/base.log
+
+podman_build() {
+  # You can add --no-cache as an option to podman_build below to rebuild all containers from scratch.
+  "$podman" build \
+    --build-arg img_version=${img_version} \
+    -v "${files_root}":/root/files:z \
+    -t godot-"$1:${img_version}" \
+    -f Dockerfile."$1" . \
+    2>&1 | tee logs/"$1".log
+}
+
+podman_build $3
